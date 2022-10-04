184404a4caef8f2608fc5f85f1261e7406f0a989
jamie-brunstad <82850533+jamie-brunstad@users.noreply.github.com>
2022-10-03 22:57:56 -0400

    Add files via upload

---
+++ SATsolvers.java
@@ -0,0 +1,240 @@
+import java.util.List;
+import java.security.cert.TrustAnchor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class SATsolvers {
+
+
+//clauses.add(new Integer[] {(int)Math.random()*10,(int)Math.random()*10 , (int)Math.random()*10});
+
+    public static void main(String[] args) {
+        ArrayList<Integer[]> clauses = new ArrayList<Integer[]>();
+        int numVars = 30;
+        for (int i = 0; i < 100; i++) {
+            Integer[] clause = new Integer[3];
+            for (int j = 0; j < 3; j++) {
+                clause[j] = ((int)(Math.random()*numVars) + 1) * ((int)(Math.random() * 2) == 1 ? 1 : -1);
+                
+            }
+            clauses.add(clause);
+        }
+        //code for printing out clauses
+        for (int i = 0; i < clauses.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" "+clauses.get(i)[j]);
+            }
+            System.out.println();
+        }
+        //now clauses has 10 clauses of size 3 with random ints -10 ....-1 .... 1 .... 10
+        double Seven8StartTime = System.nanoTime();
+        int test78Sat = Seven8SatSolver(clauses, numVars);
+        double Seven8EndTime = System.nanoTime();
+        System.out.println("7/8 Approx. : "+test78Sat + " runtime (s): " + (Seven8EndTime-Seven8StartTime)/1000000000);
+        double GSATStartTime = System.nanoTime();
+        int testGSAT = GSATsolver(clauses, numVars);
+        double GSATEndTime = System.nanoTime();
+        System.out.println("GSAT: "+testGSAT + " runtime (s): " + (GSATEndTime-GSATStartTime)/1000000000);
+        
+        //DPLL is taking too long to run & I think it is broken
+        // double DPLLStartTime = System.nanoTime();
+        // int testDPLL = DPLLsetup(clauses, numVars);
+        // double DPLLEndTime = System.nanoTime();
+        // System.out.println("completely satisfied 1/0 yes/no " + testDPLL 
+        // + " out of clauses size of " + clauses.size() 
+        // + " runtime (s): " + (DPLLEndTime - DPLLStartTime) /1000000000);
+
+
+        
+
+        //put data into lists:
+        String[] Seven8SATData = new String[2];
+        Seven8SATData[0] = String.valueOf(test78Sat);
+        Seven8SATData[1] = String.valueOf((Seven8EndTime-Seven8StartTime)/1000000000);
+
+        String[] GSATData = new String[2];
+        GSATData[0] = String.valueOf(testGSAT);
+        GSATData[1] = String.valueOf((GSATEndTime-GSATStartTime)/1000000000);
+
+        // String[] DPLLData = new String[2];
+        // DPLLData[0] = String.valueOf(testDPLL);
+        // DPLLData[1] = String.valueOf((DPLLEndTime - DPLLStartTime) /1000000000);
+
+
+        try{
+            FileWriter file = new FileWriter("SAT_output.csv", true);
+            PrintWriter writer = new PrintWriter(file);
+            writer.print(Seven8SATData[0]);
+            writer.print(",");
+            writer.print(Seven8SATData[1]);
+            writer.print(",");
+            writer.print(GSATData[0]);
+            writer.print(",");
+            writer.print(GSATData[1]);
+            writer.print(",");
+            // writer.print(DPLLData[0]);
+            // writer.print(",");
+            // writer.print(DPLLData[1]);
+            writer.println();
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Error has occurred");
+        }
+        
+    }
+    public static int Seven8SatSolver(ArrayList<Integer[]> clauses, int number_of_variables){
+        boolean[] truthtable = new boolean[number_of_variables];
+        for (int i = 0; i < truthtable.length; i++) {
+          truthtable[i] =  (int)Math.random()*2 == 1 ? true : false;
+        }
+        //now we have assigned our truth table for our clauses.
+        
+        return countSatisfiedClauses(clauses, truthtable);
+    }
+
+    public static int GSATsolver(ArrayList<Integer[]> clauses, int number_of_variables){
+        boolean[] truthtable = new boolean[number_of_variables];
+        for (int i = 0; i < truthtable.length; i++) {
+          truthtable[i] =  (int)Math.random()*2 == 1 ? true : false;
+        }
+        //weve got ourselves a truth table!!
+        int currentBest = 0;
+        for (int i = 0; i < truthtable.length; i++) {
+            truthtable[i] = !truthtable[i];
+            //see how this performs
+            int temp = countSatisfiedClauses(clauses, truthtable);
+            //if it is better than current best, make it the best
+            if(temp > currentBest){
+                currentBest = temp;
+            }
+            //revert table back to normal before next iteration
+            truthtable[i] = !truthtable[i];
+        }
+        return currentBest;
+
+    }
+
+    public static int DPLLsetup(ArrayList<Integer[]> clauses, int number_of_variables){
+        //call each branch on the tree:
+        int num_clauses_satisfied = 0;
+       
+        boolean billy = DPLLrecursion(clauses, 1, true, number_of_variables);
+        boolean is_satisfiable = DPLLrecursion(clauses, 1, false, number_of_variables);
+
+        
+
+        if(is_satisfiable || billy){
+            //able to satisfy all clauses
+            return clauses.size();
+        }else{
+            //not able to satisfy all clauses
+            return 0;
+        }
+    }
+
+    public static boolean DPLLrecursion(ArrayList<Integer[]> clauses, int j, boolean assignment, int number_of_vars){
+        ArrayList<Integer[]> newFormula = new ArrayList<Integer[]>();
+        for (int i = 0; i < clauses.size(); i++) {
+            newFormula.add(clauses.get(i));
+        }
+        //System.out.println("newformula size after copy: " + newFormula.size());
+
+        if(newFormula.size() == 0){
+            return true;
+        }
+        //if we have gone too deep down the recursion rabbit hole, get us out
+        if(j > number_of_vars){
+            return false;
+        }
+
+        
+        for (int i = 0; i < newFormula.size(); i++) {
+            for (int k = 0; k < 3; k++) {
+                if(newFormula.get(i)[k] == j){
+                    if(assignment){
+                        //create new formula, without the clause that we know can be satisfied
+                        //now we have this newFormula.
+                        //System.out.println("hello");
+                        newFormula.remove(i);
+                        break;
+                       //System.out.println("newFormula size after removing ith element: " + newFormula.size());
+                    }else{
+                        //if false, replace that with a zero
+                        //System.out.println("hell02");
+                        newFormula.get(i)[k] = 0;
+                        
+                    }
+                }
+                
+                
+            }
+        }
+
+        for (int i = 0; i < newFormula.size(); i++) {
+            int numberOfZeros = 0;
+            for (int k = 0; k < 3; k++) {
+
+                if(newFormula.get(i)[k] == 0){
+                    numberOfZeros++;
+                }
+            }
+
+            if(numberOfZeros > 2){
+                return false;
+            }
+        }
+        //System.out.println("new formula size right before recursion: " + newFormula.size());
+        boolean billy = DPLLrecursion(newFormula, j + 1, true, number_of_vars);
+        
+        boolean is_satisfiable = DPLLrecursion(newFormula, j + 1, false, number_of_vars);
+
+        
+
+        if(is_satisfiable || billy){
+            //able to satisfy all clauses
+            return true;
+        }else{
+            //not able to satisfy all clauses
+            return false;
+        }
+
+    }
+
+
+
+
+
+
+    public static int countSatisfiedClauses(ArrayList<Integer[]> clauses, boolean[] truthAssignment){
+        int numSatisfied = 0;
+        for (int i = 0; i < clauses.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+                if(truthAssignment[Math.abs(clauses.get(i)[j])-1]){
+                    //this implies that xi is true and !xi is false
+                    //check if we are dealing with a positive literal, not a negation
+                    if(clauses.get(i)[j] > 0){
+                        numSatisfied++;
+                        break;
+                    }
+                }else{
+                    //this implies that the xi is false and the !xi is true
+                    if(clauses.get(i)[j] < 0){
+                        numSatisfied++;
+                        break;
+                    }
+                    
+                }
+                
+            }
+        }
+        
+        return numSatisfied;
+    }
+    
+}
